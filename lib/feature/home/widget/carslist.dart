2e1a1985480a17c84e8100b5c2629353b9da1497
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-10 14:07:07 +0530

    map the  car card

--- lib/feature/home/widget/carslist.dart
+++ lib/feature/home/widget/carslist.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class CarList extends StatefulWidget {
@@ -8,15 +10,105 @@ class CarList extends StatefulWidget {
 }
 
 class _CarListState extends State<CarList> {
+  bool like=true;
   @override
   Widget build(BuildContext context) {
-    MediaQueryData mediaQueryData=MediaQuery.of(context);
-    return  Container(
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    return Container(
       width: mediaQueryData.size.width,
-      height: mediaQueryData.size.height*0.46,
-      decoration:const  BoxDecoration(
-        color: Colors.white,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25),)
+      height: mediaQueryData.size.height * 0.55,
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+          )),
+      child: Stack(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 14.0,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Popular car",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  "View All",
+                  style: TextStyle(fontWeight: FontWeight.w200, fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: mediaQueryData.size.height * 0.06,
+          ),
+          GridView.builder(
+              itemCount: 6,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8),
+                  child: Container(
+                    width: mediaQueryData.size.width * 0.12,
+                    height: mediaQueryData.size.height * 0.25,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Stack(
+                      children: [
+                          Positioned(
+                          top: 10,
+                          left: 10,
+                          child: GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                 like = !like;
+                                print(like);
+                              });
+                            },
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Icon(
+                                like?
+                                Icons.favorite:Icons.favorite_border,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Image.asset("assets/images/car2.png"),
+                        Positioned(
+                          top: mediaQueryData.size.height * 0.16,
+                          left: mediaQueryData.size.width * 0.06,
+                          child: const Text(
+                            "data",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Positioned(
+                          top: mediaQueryData.size.height * 0.185,
+                          left: mediaQueryData.size.width * 0.06,
+                          child:   Row(
+                            children: [
+                              const Text("\$45,590", style: TextStyle(fontWeight: FontWeight.w500),),
+                              SizedBox(width: mediaQueryData.size.width*0.05,),
+                              const Text("⭐ 4.6", style: TextStyle(fontWeight: FontWeight.w500),)
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }),
+        ],
       ),
     );
   }
