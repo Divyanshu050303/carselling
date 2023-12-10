335f118ff96554033cdbec9f4c193b24954c4bf3
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-10 14:06:19 +0530

    map the brand name and their logo

--- lib/feature/home/widget/category.dart
+++ lib/feature/home/widget/category.dart
@@ -4,12 +4,45 @@ class Category extends StatelessWidget {
   const Category({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    MediaQueryData mediaQueryData=MediaQuery.of(context);
-    return  Container(
-      width: mediaQueryData.size.width,
-      height: mediaQueryData.size.height*0.17,
-      color: Colors.black12,
-    );
+  Widget build(BuildContext context) {List<Map<String, String>> category = [
+      {"name": "Audi", "image": "assets/images/audi.png"},
+      {"name": "BMW", "image": "assets/images/bmw.png"},
+      {"name": "Bugatti", "image": "assets/images/bugatti.png"},
+      {"name": "Ferrari", "image": "assets/images/ferrari.png"},
+      {"name": "Ford", "image": "assets/images/ford.png"},
+      {"name": "Hyundai", "image": "assets/images/hyundi.png"},
+      {"name": "Lamborghini", "image": "assets/images/lambo.png"},
+      {"name": "Mahindra", "image": "assets/images/mahidra.png"},
+      {"name": "Mercedes", "image": "assets/images/mercedes.png"},
+      {"name": "porsche", "image": "assets/images/por.png"},
+      {"name": "Supra", "image": "assets/images/supra.png"},
+      {"name": "Toyota", "image": "assets/images/toyata.png"},
+    ];
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    return SizedBox(
+        width: mediaQueryData.size.width,
+        height: mediaQueryData.size.height * 0.1,
+        child: ListView.builder(
+            itemCount: category.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return SizedBox(
+                width: mediaQueryData.size.width * 0.22,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 25,
+                      child: Image.asset(
+                        category[index]['image']!,
+                      ),
+                    ),
+                    Text(
+                      category[index]['name']!,
+                      overflow: TextOverflow.ellipsis,
+                    )
+                  ],
+                ),
+              );
+            }));
   }
 }
