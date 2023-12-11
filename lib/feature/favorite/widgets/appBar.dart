6833218381f54106606d2ba0c28cdd10ed75032c
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-11 22:39:57 +0530

    favorite screen appbar

---
+++ lib/feature/favorite/widgets/appBar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppBarFav extends StatelessWidget {
+  const AppBarFav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+          width: mediaQueryData.size.width * 0.15,
+          height: mediaQueryData.size.height * 0.07,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 30,
+          ),
+        ),
+        const Text(
+          "Favorite car",
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              decoration: TextDecoration.none),
+        ),
+        Container(
+          width: mediaQueryData.size.width * 0.15,
+          height: mediaQueryData.size.height * 0.07,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: const Icon(
+            Icons.search,
+            color: Colors.black,
+            size: 26,
+          ),
+        ),
+      ],
+    );
+  }
+}
