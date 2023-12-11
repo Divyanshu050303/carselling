afc849e5f76f6fe7eea9cc08306a1248862556e2
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-11 22:39:46 +0530

    favorite screen

---
+++ lib/feature/favorite/screen/favorite.dart
@@ -0,0 +1,33 @@
+import 'dart:ffi';
+
+import 'package:carselling/feature/favorite/widgets/appBar.dart';
+import 'package:carselling/feature/favorite/widgets/favroiteCard.dart';
+import 'package:flutter/material.dart';
+
+class Favorite extends StatefulWidget {
+  const Favorite({super.key});
+
+  @override
+  State<Favorite> createState() => _FavoriteState();
+}
+
+class _FavoriteState extends State<Favorite> {
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData=MediaQuery.of(context);
+    return  Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.grey.shade300,
+      child: Column(
+        children: [
+          SizedBox(height: mediaQueryData.size.height*0.06,),
+          const AppBarFav(),
+          SizedBox(height: mediaQueryData.size.width*0.04,),
+          const FavoriteCard()
+
+        ],
+      ),
+    );
+  }
+}
