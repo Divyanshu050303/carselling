dee6e7969b783088bb0d85e31a79db5b7c1fc267
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-12 23:56:05 +0530

    Appbar setting

---
+++ lib/feature/setting/widget/appbarsetting.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppBars extends StatelessWidget {
+  const AppBars({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData=MediaQuery.of(context);
+    return Padding(
+      padding:   EdgeInsets.only(right: mediaQueryData.size.width*0.4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: mediaQueryData.size.width * 0.15,
+            height: mediaQueryData.size.height * 0.07,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          SizedBox(width: mediaQueryData.size.width*0.05,),
+          const Text(
+            "Setting",
+            style: TextStyle(
+              fontFamily: "Boboto",
+                color: Colors.black,
+                fontSize: 20,
+                decoration: TextDecoration.none),
+          ),
+
+
+        ],
+      ),
+    );
+  }
+}
