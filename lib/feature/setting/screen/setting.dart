7f5f42303ffbb4eca786781d0b8e2e67f25f20d4
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-15 22:44:33 +0530

    setting

--- lib/feature/setting/screen/setting.dart
+++ lib/feature/setting/screen/setting.dart
@@ -1,4 +1,5 @@
 import 'package:carselling/feature/setting/widget/appbarsetting.dart';
+import 'package:carselling/feature/setting/widget/cardwithradiobutton.dart';
 import 'package:carselling/feature/setting/widget/settingItemCard.dart';
 import 'package:flutter/material.dart';
 
@@ -16,8 +17,20 @@ class Setting extends StatelessWidget {
         children: [
           SizedBox(height: mediaQueryData.size.height*0.06,),
           const AppBars(),
-          SizedBox(height: mediaQueryData.size.height*0.07,),
-          const SettingCard()
+          SizedBox(height: mediaQueryData.size.height*0.05,),
+          const SettingCard(iconData: Icons.calendar_month,title: "Appointment Reminder",),
+          SizedBox(height: mediaQueryData.size.height*0.03,),
+          const SettingCard(iconData: Icons.location_on,title: "Preferred Location",),
+          SizedBox(height: mediaQueryData.size.height*0.03,),
+          const SettingCard(iconData: Icons.favorite,title: "FAQs",),
+          SizedBox(height: mediaQueryData.size.height*0.03,),
+          const SettingCard(iconData: Icons.phone_in_talk,title: "Contact Support",),
+          SizedBox(height: mediaQueryData.size.height*0.03,),
+          const SettingCard(iconData: Icons.star,title: "Rating & Reviews",),
+          SizedBox(height: mediaQueryData.size.height*0.03,),
+          const CardWithButton(iconData: Icons.lock_clock,title: "Incentives",),
+          SizedBox(height: mediaQueryData.size.height*0.03,),
+          const CardWithButton(iconData: Icons.notifications,title: "Notification",),
 
 
         ],
