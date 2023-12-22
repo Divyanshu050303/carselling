055806a9fd58d89900e4bfd3c71c60e289171e9f
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-22 20:42:20 +0530

    add the navigator

--- lib/feature/car_details/widget/appBar.dart
+++ lib/feature/car_details/widget/appBar.dart
@@ -1,3 +1,4 @@
+import 'package:carselling/NavBar.dart';
 import 'package:flutter/material.dart';
 
 class CarDetailsAppBar extends StatelessWidget {
@@ -9,26 +10,34 @@ class CarDetailsAppBar extends StatelessWidget {
     return  Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Container(
-          width: mediaQueryData.size.width * 0.13,
-          height: mediaQueryData.size.height * 0.06,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(100),
-          ),
-          child: const Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-            size: 30,
+        GestureDetector(
+          onTap: (){
+            Navigator.pop(context, MaterialPageRoute(builder: (context)=>const NavBar()));
+          },
+          child: Container(
+            width: mediaQueryData.size.width * 0.13,
+            height: mediaQueryData.size.height * 0.06,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              size: 30,
+            ),
           ),
         ),
-        const Text(
-          "Car Details",
-          style: TextStyle(
-            fontFamily: "Roboto",
-              color: Colors.black,
-              fontSize: 18,
-              decoration: TextDecoration.none),
+        const Padding(
+          padding:   EdgeInsets.symmetric(horizontal: 28.0),
+          child:   Text(
+            "Car Details",
+            style: TextStyle(
+              fontFamily: "Roboto",
+                color: Colors.black,
+                fontSize: 18,
+                decoration: TextDecoration.none),
+          ),
         ),
         Container(
           width: mediaQueryData.size.width * 0.13,
