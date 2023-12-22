21e24846c3abe67dcd47042e2fe9c36c729196d5
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-12-22 20:43:28 +0530

    adjust the space between the widget

--- lib/feature/setting/screen/setting.dart
+++ lib/feature/setting/screen/setting.dart
@@ -12,29 +12,31 @@ class Setting extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      color: Colors.grey.shade300,
-      child: Column(
-        children: [
-          SizedBox(height: mediaQueryData.size.height*0.06,),
-          const AppBars(),
-          SizedBox(height: mediaQueryData.size.height*0.05,),
-          const SettingCard(iconData: Icons.calendar_month,title: "Appointment Reminder",),
-          SizedBox(height: mediaQueryData.size.height*0.03,),
-          const SettingCard(iconData: Icons.location_on,title: "Preferred Location",),
-          SizedBox(height: mediaQueryData.size.height*0.03,),
-          const SettingCard(iconData: Icons.favorite,title: "FAQs",),
-          SizedBox(height: mediaQueryData.size.height*0.03,),
-          const SettingCard(iconData: Icons.phone_in_talk,title: "Contact Support",),
-          SizedBox(height: mediaQueryData.size.height*0.03,),
-          const SettingCard(iconData: Icons.star,title: "Rating & Reviews",),
-          SizedBox(height: mediaQueryData.size.height*0.03,),
-          const CardWithButton(iconData: Icons.lock_clock,title: "Incentives",),
-          SizedBox(height: mediaQueryData.size.height*0.03,),
-          const CardWithButton(iconData: Icons.notifications,title: "Notification",),
-
-
-        ],
+      color: Colors.grey.shade200,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: mediaQueryData.size.height*0.04,),
+            const AppBars(),
+            SizedBox(height: mediaQueryData.size.height*0.03,),
+            const SettingCard(iconData: Icons.calendar_month,title: "Appointment Reminder",),
+            SizedBox(height: mediaQueryData.size.height*0.03,),
+            const SettingCard(iconData: Icons.location_on,title: "Preferred Location",),
+            SizedBox(height: mediaQueryData.size.height*0.03,),
+            const SettingCard(iconData: Icons.favorite,title: "FAQs",),
+            SizedBox(height: mediaQueryData.size.height*0.03,),
+            const SettingCard(iconData: Icons.phone_in_talk,title: "Contact Support",),
+            SizedBox(height: mediaQueryData.size.height*0.03,),
+            const SettingCard(iconData: Icons.star,title: "Rating & Reviews",),
+            SizedBox(height: mediaQueryData.size.height*0.03,),
+            const CardWithButton(iconData: Icons.lock_clock,title: "Incentives  ",),
+            SizedBox(height: mediaQueryData.size.height*0.03,),
+            const CardWithButton(iconData: Icons.notifications,title: "Notification",),
+        
+        
+          ],
+        ),
       ),
-    );;
+    );
   }
 }
